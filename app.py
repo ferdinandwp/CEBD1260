69940f55c414e618a62a96987b02dfaadbe98238
Ferdinand <ferdinand@Ferdinands-MacBook-Pro.local>
2019-11-30 18:43:06 -0500

    mini update

--- app.py
+++ app.py
@@ -11,15 +11,25 @@ sys.path.append('..')
 # from processed_script.prev_app import preprocessing_prev_app
 # from processed_script.POS_CASH_balance import preprocessing_POS_CASH_balance
 from processed_script.main import preprocessing_main
+# from processed_script.bureau import preprocessing_bureau
+
 
 # ipay_df = preprocessing_ipay()
 # cc_balance_df = preprocessing_cc_balance()
 # prev_app_df = preprocessing_prev_app()
 # POS_CASH_balance_df = preprocessing_POS_CASH_balance()
 main_df = preprocessing_main()
+# bureau_df = preprocessing_bureau()
+
+# *************** Data Joining *************** #
+
+
+
+
+
+# *************** Algorithm *************** #
 
 # Random Forest Classification
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import KFold
 from sklearn.datasets import make_classification
@@ -34,7 +44,7 @@ y_train = main_df['TARGET']
 for i,(tr_idx, val_idx) in enumerate(kf.split(X_train,y_train)):
     print('Fold :{}'.format(i))
     tr_X = X_train.loc[tr_idx]  # training for this loop
-    tr_y = y_train[tr_idx] #
+    tr_y = y_train[tr_idx] 
     val_X = X_train.loc[val_idx]# validation data for this loop
     val_y = y_train[val_idx]
     
@@ -54,9 +64,4 @@ for i,(tr_idx, val_idx) in enumerate(kf.split(X_train,y_train)):
                 verbose=-1
                 )
     model.fit(tr_X, tr_y, eval_set=[(tr_X, tr_y), (val_X, val_y)], eval_metric = 'binary_logloss', verbose=100, early_stopping_rounds= 200)
-    #     pred_val_y = model.predict_proba(va_X,num_iteration=model.best_iteration_)[:,1]
-
-
-
-
-
+    # pred_val_y = model.predict_proba(va_X,num_iteration=model.best_iteration_)[:,1]
